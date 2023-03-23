d55cb0966aacef7bfe6857c961eeb34fad2fb827
Maksim Kudaev <kudaev.m@nakukop.com>
2023-03-23 18:02:15 +0300

    feat: add user actions and reformat the code

--- 1-cli-todo-app/main.py
+++ 1-cli-todo-app/main.py
@@ -1,9 +1,25 @@
-user_prompt: str = 'Enter a todo: '
-todos_count_prompt: str = 'Enter count of todos you want: '
+todos: list = []
 
-todos_count: int = int(input(todos_count_prompt))
-todos: list = [input(user_prompt) for _ in range(todos_count)]
-todos_length = len(todos)
 
-print(*[f"Todo: '{todo}'" for todo in todos], sep='\n')
-print(f"Todos length: {todos_length}")
+while True:
+    user_action: str = input('Type "add", "show" or "exit": ')
+    user_action = user_action.lower().strip()
+
+    match user_action:
+        case 'add' | 'create':
+            todo: str = input("Enter a todo: ")
+            todos.append(todo)
+        case 'show' | 'display':
+            if len(todos) >= 1:
+                print('Todos now:')
+
+                for todo in enumerate(todos, start=1):
+                    print(f"{todo[0]}: {todo[1].capitalize()}.")
+            else:
+                print('Nothing to show.')
+        case 'exit' | 'quit':
+            break
+        case _:
+            print('Hey, you entered an unknown command.')
+
+print('Bye!')
