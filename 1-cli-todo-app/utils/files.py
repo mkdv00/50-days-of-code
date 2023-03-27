09d789b56c4f886563f2dcf322430b80faafad0e
Maksim Kudaev <kudaev.m@nakukop.com>
2023-03-27 19:01:15 +0300

    refactor: add docstrings to files module

--- 1-cli-todo-app/utils/files.py
+++ 1-cli-todo-app/utils/files.py
@@ -1,4 +1,5 @@
 def read_todo(filename):
+    """Read todos.txt file and return list of todos"""
     with open(filename, 'r') as file:
         todos = file.readlines()
 
@@ -6,5 +7,6 @@ def read_todo(filename):
 
 
 def write_todo(filename, content):
+    """Overwrite todos.txt file with new todos list"""
     with open(filename, 'w') as file:
         file.writelines(content)
