b4d2b7b9bf20c54a722d0d329501e867fa1325b7
Maksim Kudaev <kudaev.m@nakukop.com>
2023-04-04 12:42:38 +0300

    feat: colorize any photo

--- 3-todo-app-web/camera.py
+++ 3-todo-app-web/camera.py
@@ -1,15 +1,19 @@
 import streamlit as st
 from PIL import Image
 
-# Start the camera
-camera_image = st.camera_input('Camera')
+st.subheader("Color to Grayscale Converter")
+
+uploaded_image = st.file_uploader("Upload Image")
+
+with st.expander("Start camera"):
+    camera_image = st.camera_input("Camera")
 
 if camera_image:
-    # Create a pillow instance
     img = Image.open(camera_image)
+    gray_camera_img = img.convert('L')
+    st.image(gray_camera_img)
 
-    # Convert the pillow image to grayscale
-    gray_img = img.convert('L')
-
-    # Render the grayscale image on the webpage
-    st.image(gray_img)
+if uploaded_image:
+    img = Image.open(uploaded_image)
+    gray_upload_img = img.convert('L')
+    st.image(gray_upload_img)
