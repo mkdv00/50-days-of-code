4de0f08e0a6f928fbbe137a5bc2b8f19aabd758d
Maksim Kudaev <kudaev.m@nakukop.com>
2023-04-04 15:38:54 +0300

    refactor: remove camera to main file

--- 3-todo-app-web/camera.py
+++ 3-todo-app-web/camera.py
@@ -1,6 +1,29 @@
 import streamlit as st
+from PIL import Image, ImageFilter
 
-from camera.filters import apply_filter
+
+def apply_filter(option, photo):
+    img = Image.open(photo)
+
+    match option:
+        case 'Blur':
+            img = img.filter(ImageFilter.BLUR)
+        case 'Contour':
+            img = img.filter(ImageFilter.CONTOUR)
+        case 'Detail':
+            img = img.filter(ImageFilter.DETAIL)
+        case 'Edge Enhance':
+            img = img.filter(ImageFilter.EDGE_ENHANCE)
+        case 'Emboss':
+            img = img.filter(ImageFilter.EMBOSS)
+        case 'Find Edges':
+            img = img.filter(ImageFilter.FIND_EDGES)
+        case 'Sharpen':
+            img = img.filter(ImageFilter.SHARPEN)
+        case 'Gray':
+            img = img.convert('L')
+
+    return img
 
 
 def main():
