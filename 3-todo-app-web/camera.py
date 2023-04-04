68ad12c54fbf659eb980c4aaf368acf078565682
Maksim Kudaev <kudaev.m@nakukop.com>
2023-04-04 15:37:07 +0300

    feat: added filters to images

--- 3-todo-app-web/camera.py
+++ 3-todo-app-web/camera.py
@@ -1,19 +1,28 @@
 import streamlit as st
-from PIL import Image
 
-st.subheader("Color to Grayscale Converter")
+from camera.filters import apply_filter
 
-uploaded_image = st.file_uploader("Upload Image")
 
-with st.expander("Start camera"):
-    camera_image = st.camera_input("Camera")
+def main():
+    st.subheader("Image Filters")
 
-if camera_image:
-    img = Image.open(camera_image)
-    gray_camera_img = img.convert('L')
-    st.image(gray_camera_img)
+    uploaded_image = st.file_uploader("Upload Image")
 
-if uploaded_image:
-    img = Image.open(uploaded_image)
-    gray_upload_img = img.convert('L')
-    st.image(gray_upload_img)
+    options = ['Gray', 'Blur', 'Contour', 'Sharpen', 'Find Edges', 'Emboss', 'Edge Enhance', 'Detail']
+    selected_option = st.selectbox("Choose a filter", options)
+    st.write("You selected:", selected_option)
+
+    with st.expander("Start camera"):
+        camera_image = st.camera_input("Camera")
+
+    if camera_image:
+        img = apply_filter(selected_option, camera_image)
+        st.image(img, caption="Filtered camera Image", use_column_width=True)
+
+    if uploaded_image:
+        img = apply_filter(selected_option, uploaded_image)
+        st.image(img, caption="Filtered Image", use_column_width=True)
+
+
+if __name__ == '__main__':
+    main()
