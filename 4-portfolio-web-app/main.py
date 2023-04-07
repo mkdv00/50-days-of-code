c3baebdf8a1344a33b2f50338ee4dde260720d2b
Maksim Kudaev <kudaev.m@nakukop.com>
2023-04-07 18:59:20 +0300

    feat: added description, source code and iamges

--- 4-portfolio-web-app/main.py
+++ 4-portfolio-web-app/main.py
@@ -13,19 +13,19 @@ with col_2:
     st.title('Maksim Kudaev')
     st.info(description)
 
-content_2 = """
-Bellow you can find some of the apps I have built in Python.
-"""
-st.write(content_2)
-
-col_3, col_4 = st.columns(2)
-
+col_3, empty_column, col_4 = st.columns([1.5, 0.5, 1.5])
 df = pandas.read_csv('data.csv', sep=';')
 
 with col_3:
     for index, row in df[:10].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.write(f"[Source code]({row['url']})")
+        st.image('images/' + row['image'])
 
 with col_4:
     for index, row in df[10:].iterrows():
         st.header(row['title'])
+        st.write(row['description'])
+        st.write(f"[Source code]({row['url']})")
+        st.image('images/' + row['image'])
